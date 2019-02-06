53c3af9c30f231765258ed14cda4f044c0e2d0ee
gargaryan <45004188+gargaryan@users.noreply.github.com>
2019-02-06 07:45:18 +0530

    Update program9.py

--- program9.py
+++ program9.py
@@ -1,7 +1,7 @@
 #program started
 #taking input
-height = eval(input("Enter height of rectangle"))
-base = eval(input("Enter base of rectangle"))
+height = eval(input("Enter height of right angle triangle"))
+base = eval(input("Enter base of right angle triangle"))
 hypotenuse = (height**2 + base**2)**0.5      #method of evaluation of hypotenuse
-print("the hypotenuse of triangle is ",hypotenuse)
+print("the hypotenuse of  right angle triangle is ",hypotenuse)
 #program ends
